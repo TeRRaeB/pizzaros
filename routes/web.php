1a549a80cd1dddc9c2daa9adb8a4e31f40344cfb
TeRRaeB <mihalyovvalik@gmail.com>
2018-11-30 18:29:02 +0200

    Фикс pages/products

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('pages/index');
 })->name('index');
-Route::get('/pages/products', function () {
+Route::get('pages/products', function () {
     return view('pages/products');
 })->name('products');
